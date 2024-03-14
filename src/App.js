758cb07d6baa66f8fbb7ba23576a95eb56d117f3
Rajamanickam <erajamanickam72@gmail.com>
2024-03-14 09:55:19 +0530

    GA Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React, { useEffect } from 'react';
 import 'animate.css';
+import { BrowserRouter as Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import PositionBg from './Components/PositionBg';
@@ -12,21 +15,37 @@ import About from './Components/About';
 import Projects from './Components/Projects';
 
 
+ReactGA.initialize('G-P52FG9L651');
+
+
 function App() {
+
+  const TrackPageViews = () => {
+    const location = useLocation();
+    useEffect(() => {
+      ReactGA.pageview(location.pathname);
+      console.log('GA Processed');
+    }, [location]);
+    return null;
+  };
+
   return (
-    <div className='bg-slate-800'>
-      <PositionBg />
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Experience />
-      <Projects />
-      <Video />
-      <Cta />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <div className='bg-slate-800'>
+        <TrackPageViews />
+        <PositionBg />
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Video />
+        <Cta />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   );
 }
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,41 +2,65 @@ import React from 'react';
 import doctoruiIcon from '../images/doctorui-icon.png'
 import loginuiIcon from '../images/login-icon.png'
 import dashboarduiIcon from '../images/dashboardui-icon.png'
+import accordionIcon from '../images/accordion.png'
+import tabsIcon from '../images/tabsui.png'
 import bloguiIcon from '../images/blogui-icon.png'
 import { UilAngleDoubleRight } from '@iconscout/react-unicons'
 
 
 const Projects = () => {
     const projects = [
-        {
-            id: 1,
-            icon: doctoruiIcon,
-            title: 'Doctor Appointment',
-            description: 'A doctor appointment app can help doctors to manage their patients...',
-            link: 'https://github.com/erajamanickam/booking-appointment',
-        },
-        {
-            id: 2,
-            icon: bloguiIcon,
-            title: 'Blog Application Node',
-            description: 'Web application that allows users to create, manage, and publish their own...',
-            link: 'https://github.com/erajamanickam/blog-application-nodejs',
-        },
+        // {
+        //     id: 1,
+        //     icon: doctoruiIcon,
+        //     title: 'Doctor Appointment',
+        //     description: 'A doctor appointment app can help doctors to manage their patients...',
+        //     link: 'https://github.com/erajamanickam/booking-appointment',
+        // },
+        // {
+        //     id: 2,
+        //     icon: bloguiIcon,
+        //     title: 'Blog Application Node',
+        //     description: 'Web application that allows users to create, manage, and publish their own...',
+        //     link: 'https://github.com/erajamanickam/blog-application-nodejs',
+        // },
         {
             id: 3,
             icon: dashboarduiIcon,
             title: 'React Dashboard',
-            description: 'Developed to streamline task management within agile teams...',
+            description: 'Visual Task Board layout, robust drag-and-drop functionality...',
             link: 'https://github.com/erajamanickam/Task-Management-Dashboard',
         },
+        // {
+        //     id: 4,
+        //     icon: loginuiIcon,
+        //     title: 'Glossy Login UI',
+        //     description: 'A login screen is a user interface that is displayed when a user...',
+        //     link: 'https://codepen.io/erajamanickam/pen/NWeRZzx',
+        // },
         {
-            id: 4,
-            icon: loginuiIcon,
-            title: 'Glossy Login UI',
-            description: 'A login screen is a user interface that is displayed when a user...',
-            link: 'https://codepen.io/erajamanickam/pen/NWeRZzx',
+            id: 5,
+            icon: accordionIcon,
+            title: 'React Accordion',
+            description: 'Easily organize and display content in collapsible sections...',
+            link: 'https://github.com/erajamanickam/React-Accordion',
         },
+        {
+            id: 6,
+            icon: tabsIcon,
+            title: 'React Tabs',
+            description: 'Implementation of tabbed navigation systems within React applications...',
+            link: 'https://github.com/erajamanickam/React-Tabs',
+        },
+        {
+            id: 7,
+            icon: loginuiIcon,
+            title: 'React Form Validation',
+            description: 'Data integrity and user input accuracy with customizable validation rules...',
+            link: 'https://github.com/erajamanickam/React-Form-Validation',
+        }
     ];
+    
 
     return (
         <section className="py-12 z-20 relative" id='projects'>
